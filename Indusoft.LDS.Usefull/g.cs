9ae6923d480f9a9363f9268d1ddcb86a5d0cacc3
Алексей Назаров <nazarovalexey94@gmail.com>
2021-12-23 09:44:00 +0300

    Изменена платформа на Frmwrk 4. Добавлен WriteBounds

--- Indusoft.LDS.Usefull/g.cs
+++ Indusoft.LDS.Usefull/g.cs
@@ -665,6 +665,106 @@ namespace Indusoft.LDS.Usefull
             return analogTechTest.Measures[index].Value;
         }
 
+        /// <summary>
+        /// Записать границы определения показателей из расчета. Префиксы AnalogTechTest.Prefix по умолчанию: LessOrEqual, None, MoreOrEqual
+        /// </summary>
+        /// <param name="Low">Нижнаяя граница</param>
+        /// <param name="LowDigits">Количество знаков после запятой для нижней границы</param>
+        /// <param name="High">Верхняя граница</param>
+        /// <param name="HighDigits">Количество знаков после запятой для верхней границы</param>
+        /// <param name="techTests">Показатели, для которых проверяется граница</param>
+        public static void WriteBounds(double Low, byte LowDigits, double High, byte HighDigits, params AnalogTechTest[] techTests)
+        {
+            string lowFormat = LowDigits == 0 ? "" : ".";
+            for (int i = 0; i < LowDigits; i++) { lowFormat += "0"; }
+
+            string highFormat = HighDigits == 0 ? "" : ".";
+            for (int i = 0; i < HighDigits; i++) { highFormat += "0"; }
+
+            foreach (AnalogTechTest techTest in techTests)
+            {
+                if (techTest.Exists)
+                {
+                    if (techTest.Value < Low)
+                    {
+                        techTest.SetLessOrEqual();
+                        techTest.FValue = String.Format("менее {0}", Low.ToString(lowFormat));
+                    }
+
+                    if (Low <= techTest.Value && techTest.Value <= High)
+                    {
+                        techTest.SetPrefix(ValuePrefix2.None);
+                    }
+
+                    if (High < techTest.Value)
+                    {
+                        techTest.SetMoreOrEqual();
+                        techTest.FValue = String.Format("более {0}", High.ToString(highFormat));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Записать границы определения показателей из расчета.
+        /// </summary>
+        /// <param name="LowPrefix"></param>
+        /// <param name="Low">Нижнаяя граница</param>
+        /// <param name="LowDigits">Количество знаков после запятой для нижней границы</param>
+        /// <param name="HighPrefix"></param>
+        /// <param name="High">Верхняя граница</param>
+        /// <param name="HighDigits">Количество знаков после запятой для верхней границы</param>
+        /// <param name="techTests">Показатели, для которых проверяется граница</param>
+        public static void WriteBounds(ValuePrefix2 LowPrefix, double Low, int LowDigits, ValuePrefix2 HighPrefix, double High, int HighDigits, params AnalogTechTest[] techTests)
+        {
+            string lowFormat = LowDigits == 0 ? "" : ".";
+            for (int i = 0; i < LowDigits; i++) { lowFormat += "0"; }
+
+            string highFormat = HighDigits == 0 ? "" : ".";
+            for (int i = 0; i < HighDigits; i++) { highFormat += "0"; }
+
+            foreach (AnalogTechTest techTest in techTests)
+            {
+                if (techTest.Exists)
+                {
+                    if (techTest.Value < Low)
+                    {
+                        //Вместо простого выставления префикса или SetPrefix(префикс) используются именные методы на всякий случай, если в них добавят какую-нибудь логику в будущем
+                        switch (LowPrefix)
+                        {
+                            case (ValuePrefix2.Less):
+                                techTest.SetLess(); break;
+                            case ValuePrefix2.LessOrEqual:
+                                techTest.SetLessOrEqual(); break;
+                            default:
+                                throw new Exception("Неверный формат префикса");
+                        }
+                        techTest.FValue = String.Format("менее {0}", Low.ToString(lowFormat));
+                    }
+
+                    if (Low <= techTest.Value && techTest.Value <= High)
+                    {
+                        techTest.SetPrefix(ValuePrefix2.None);
+                    }
+
+                    if (High < techTest.Value)
+                    {
+                        //Вместо простого выставления префикса или SetPrefix(префикс) используются именные методы на всякий случай, если в них добавят какую-нибудь логику в будущем
+                        switch (HighPrefix)
+                        {
+                            case (ValuePrefix2.More):
+                                techTest.SetMore(); break;
+                            case ValuePrefix2.MoreOrEqual:
+                                techTest.SetMoreOrEqual(); break;
+                            default:
+                                throw new Exception("Неверный формат префикса");
+                        }
+                        techTest.FValue = String.Format("более {0}", High.ToString(highFormat));
+                    }
+                }
+            }
+        }
+
         private static void NormalizeValue(AnalogTechTest analogTechTest, IScriptSession session)
         {
             var cdm = ((IGenericServiceProvider)session).GetService<IRepositoryDataServiceFactory>();
